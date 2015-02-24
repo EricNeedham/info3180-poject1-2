f788daa4533ad61b6d22ebd8d7ab030ce99c512a
Eric Needham <needham_eric@hotmail.com>
2015-02-24 04:16:07 +0000

    models.py update

--- app/models.py
+++ app/models.py
@@ -1,13 +1,23 @@
-from app import db
+from . import db
+from marshmallow import schema 
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True)
-  email = db.Column(db.String(120), unique=True)
-
-  def __init__(self, username, email):
-    self.username = username
-    self.email = email
-
+  fisrt_name = db.Column(db.String(80))
+  last_name = db.Column(db.String(80))
+  age = db.Column(db.Integer)
+  image = db.Column(db.String(120))
+  
+  def __init__(self, fisrt_name, last_name, age, sex, image):
+    self.fisrt_name = fisrt_name
+    self.last_name = last_name
+    self.age = age
+    self.image = image
+    
   def __repr__(self):
-    return '<User %r>' % self.username
+    return '<User %r>' % self.fisrt_name
+class UserSchema(schema):
+  formatted_name = fields.Method("format_name")
+  
+class Meta:
+  fields = ('fisrt_name','last_name','age','image')
